188f2a04dfa8d267de48f6f0bd1ffe79a9bca74f
Sasikumar Palanisamy <sasikumarp@caresoftglobal.com>
2022-02-18 16:31:26 +0530

    Sasi added

--- mySqltestor1/Controllers/loginController.cs
+++ mySqltestor1/Controllers/loginController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace mySqltestor1.Controllers
@@ -24,6 +25,57 @@ namespace mySqltestor1.Controllers
         public IEnumerable<admin_login> GetCustomerDetails()        
         {
             return _context.Admin_login.ToList();
+        }
+
+         [HttpGet("GetAccountNumber")]
+        public admin_login GetAccountNumber(long AccountNumber)
+        {
+            return _context.Admin_login.Find(AccountNumber);
+        }
+        [HttpPost("InsertCustomer")]
+        public IActionResult InsertCustomer([FromBody] admin_login context)
+        {
+            if (context.ID.ToString() != "")
+            {
+                _context.Admin_login.Add(context);
+                _context.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPut("UpdateCustomer")]
+        public IActionResult UpdateCustomer([FromBody] admin_login context)
+        {
+            if (context.ID.ToString() != "")
+            {
+                _context.Entry(context).State = EntityState.Modified;
+                _context.SaveChanges();
+                return Ok("Customer Details updated successfully!");
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpDelete("DeleteCustomer")]
+        public IActionResult DeleteCustomer(int ID)
+        {
+            //select * from CustomerDetails where AccountNumber=123
+            var result = _context.Admin_login.Find(ID);
+            if (result != null)
+            {
+                _context.Admin_login.Remove(result);
+                _context.SaveChanges();
+                return Ok("Deleted Successfully");
+            }
+            else
+            {
+                return NotFound("Invalid Account number");
+            }
+
         }
        // [HttpGet("/api/Companies/{id}")]
        // public dynamic Get(int id) => _context.Admin_login.SingleOrDefault(c => c.ID == id);
